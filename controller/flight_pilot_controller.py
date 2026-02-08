7b73fb0d875f1f6f3ea1c866caeb506abbeb02b3
dc2339 <dc2339@bath.ac.uk>
2026-02-08 17:58:56 +0000

    user input sanitized over assign pilot feature

--- controller/flight_pilot_controller.py
+++ controller/flight_pilot_controller.py
@@ -1,10 +1,13 @@
 from repository.flight_pilot_repository import assign_pilot_to_flight
+from helper_validator import get_int
 
 def assign_pilot():
+    print("\n--- Assign Pilot To Flight ---")
 
-    flight_id = input("Enter Flight ID: ")
-    pilot_id = input("Enter Pilot ID: ")
+    flight_id = get_int("Enter Flight ID: ")
+    pilot_id = get_int("Enter Pilot ID: ")
 
-    assign_pilot_to_flight(flight_id, pilot_id)
-
-    print("Pilot successfully assigned to flight")
+    try:
+        assign_pilot_to_flight(flight_id, pilot_id)
+    except Exception as e:
+        print("Failed to assign pilot:", e)
--- repository/flight_pilot_repository.py
+++ repository/flight_pilot_repository.py
@@ -1,14 +1,20 @@
 from database_connection import get_connection
 
 def assign_pilot_to_flight(flight_id, pilot_id):
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
 
-    conn = get_connection()
-    cur = conn.cursor()
+        cur.execute("""
+            INSERT INTO FlightPilot (FlightId, PilotId)
+            VALUES (?, ?)
+        """, (flight_id, pilot_id))
 
-    cur.execute("""
-        INSERT INTO FlightPilot (FlightId, PilotId)
-        VALUES (?, ?)
-    """, (flight_id, pilot_id))
+        conn.commit()
+        print("Pilot assigned to flight successfully.")
 
-    conn.commit()
-    conn.close()
+    except Exception as e:
+        print("Failed to assign pilot to flight:", e)
+
+    finally:
+        conn.close()
